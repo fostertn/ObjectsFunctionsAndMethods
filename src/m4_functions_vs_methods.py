2cbdbb36ed8e54a35193e13e4c09b05c3a5b297d
fostertn <fostertn@rose-hulman.edu>
2017-11-29 10:19:40 -0500

    completed m3

--- src/m4_functions_vs_methods.py
+++ src/m4_functions_vs_methods.py
@@ -4,12 +4,12 @@ Demonstrates using (calling) FUNCTIONS and using (calling) METHODS:
   -- how they differ.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Tyler Foster.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   READ this comment, ASKING QUESTIONS as needed to understand it.
 #
 #   For objects that are CONSTRUCTED, we use the DOT notation
@@ -162,6 +162,13 @@ def turtle3():
 
 
 def try_methods():
+    dog = rg.SimpleTurtle()
+    dog.pen = rg.Pen('brown',5)
+    dog.forward(150)
+    dog.left(90)
+    dog.forward(50)
+    dog.backward(100)
+
     """
     Constructs a SimpleTurtle and sets its   pen   to a new rg.Pen
     that is 'brown' with thickness 5.
@@ -172,7 +179,7 @@ def try_methods():
       -- backward  100 units
     """
     ####################################################################
-    # TODO: 3. Implement this function, per its doc-string above.
+    # DONE: 3. Implement this function, per its doc-string above.
     #    Put a statement in   main   to test this function
     #    (by calling this function).
     ####################################################################
